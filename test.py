0b78073c97670260819b96e49f37a82b1e8f5e9a
wltschmrz <gun0lee@korea.ac.kr>
2025-02-04 02:44:33 +0900

    new file:   src_audioldm/audioldm.py 	modified:   test.py

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
+from diffusers import AudioLDMPipeline
+
+# hf_pipe = AudioLDMPipeline.from_pretrained(pretrained_model_name_or_path="cvssp/audioldm-s-full")
+
+# print("UNet config:", hf_pipe.unet.config)
+# print("\nText Encoder config:", hf_pipe.text_encoder.config)
+# print("\nVAE config:", hf_pipe.vae.config)
+
 import torch
 
-print(torch.version.cuda)
-#
+repo_id = "cvssp/audioldm"
+pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "Techno music with a strong, upbeat tempo and high melodic riffs"
+audio = pipe(prompt, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
+print(audio.shape)
