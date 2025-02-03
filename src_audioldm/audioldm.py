caac0e32ce29fbd259ba689707d326000b00be94
wltschmrz <gun0lee@korea.ac.kr>
2025-02-04 05:33:08 +0900

    modified:   .gitignore 	modified:   README.md 	modified:   src_audioldm/audioldm.py 	modified:   test.py

--- src_audioldm/audioldm.py
+++ src_audioldm/audioldm.py
@@ -1,62 +1,59 @@
 from typing import Union, List, Optional
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from diffusers import (
    AutoencoderKL,
    PNDMScheduler,
-   StableDiffusionPipeline,
+   AudioLDMPipeline,
    UNet2DConditionModel,
 )
 from transformers import (
-   CLIPTextModel,
-   CLIPTokenizer,
+   ClapTextModelWithProjection,
+   RobertaTokenizer,
+   SpeechT5HifiGan,
    logging,
 )
 
 # Suppress partial model loading warning
 logging.set_verbosity_error()
+logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
-class StableDiffusion(nn.Module):
-    VAE_SCALING_FACTOR = 0.18215
-
-    def __init__(self, device='cuda', checkpoint_path="CompVis/stable-diffusion-v1-4"):
+class AudioLDM(nn.Module):
+    
+    def __init__(self, device='cuda', repo_id="cvssp/audioldm"):
         super().__init__()
         self.device = torch.device(device)
         self.num_train_timesteps = 1000
         self.min_step = int(self.num_train_timesteps * 0.02)
         self.max_step = int(self.num_train_timesteps * 0.98)
-        
-        print('[INFO] sd.py: loading stable diffusion...please make sure you have run `huggingface-cli login`.')
-        
+                
         # Initialize pipeline with PNDM scheduler (load from pipeline. let us use dreambooth models.)
-        pipe = StableDiffusionPipeline.from_pretrained(checkpoint_path, torch_dtype=torch.float)
-        pipe.safety_checker = lambda images, _: (images, False)  # Disable the NSFW checker (slows things down)
+        pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
         pipe.scheduler=PNDMScheduler(beta_start=0.00085,beta_end=0.012,beta_schedule="scaled_linear",num_train_timesteps=self.num_train_timesteps)
         
         # Setup components and move to device
         self.pipe = pipe
         self.components = {
             'vae': (pipe.vae, AutoencoderKL),
-            'tokenizer': (pipe.tokenizer, CLIPTokenizer),
-            'text_encoder': (pipe.text_encoder, CLIPTextModel),
+            'tokenizer': (pipe.tokenizer, RobertaTokenizer),
+            'text_encoder': (pipe.text_encoder, ClapTextModelWithProjection),
             'unet': (pipe.unet, UNet2DConditionModel),
+            'vocoder': (pipe.vocoder, SpeechT5HifiGan),
             'scheduler': (pipe.scheduler, PNDMScheduler)
         }
         
         # Initialize and validate components
         for name, (component, expected_type) in self.components.items():
-            if name in ['vae', 'text_encoder', 'unet']:
+            if name in ['vae', 'text_encoder', 'unet', 'vocoder']:
                 component = component.to(self.device)
             assert isinstance(component, expected_type), f"{name} type mismatch: {type(component)}"
             setattr(self, name, component)
         
         self.uncond_text = ''
-        self.checkpoint_path = checkpoint_path
-        self.alphas = self.scheduler.alphas_cumprod.to(self.device) # for convenience
-        print(f'[INFO] sd.py: loaded stable diffusion!')
+        self.checkpoint_path = repo_id
+        self.alphas_cumprod = self.scheduler.alphas_cumprod.to(self.device) # for convenience
+        print(f'[INFO] audioldm.py: loaded AudioLDM!')
 
     def get_text_embeddings(self, prompts: Union[str, List[str]]) -> torch.Tensor:
         prompts = [prompts] if isinstance(prompts, str) else prompts
@@ -80,6 +77,57 @@ class StableDiffusion(nn.Module):
         
         return torch.cat([uncond_embeddings, text_embeddings])  # First B rows: uncond, Last B rows: cond
 
+    def _encode_prompt(self, prompts: Union[str, List[str]], device, do_classifier_free_guidance=True) -> torch.Tensor:
+        
+        # 1. Batch size 결정
+        if prompts is not None and isinstance(prompts, str):
+            batch_size = 1
+        elif prompts is not None and isinstance(prompts, list):
+            batch_size = len(prompts)
+        else:
+            batch_size = prompt_embeds.shape[0]
+
+        # 2. Prompt embedding 생성
+        text_inputs = self.tokenizer(
+            prompts,
+            padding="max_length",
+            max_length=self.tokenizer.model_max_length,
+            truncation=True,
+            return_tensors="pt",
+        )
+        text_input_ids, attention_mask = text_inputs.input_ids.to(device), text_inputs.attention_mask.to(device)
+
+        # Truncation 경고
+        untruncated_ids = self.tokenizer(prompts, padding="longest", return_tensors="pt").input_ids
+        if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
+            removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
+            logger.warning(f"The following part of your input was truncated because CLAP can only handle sequences up to {self.tokenizer.model_max_length} tokens: {removed_text}")
+
+        # Text embedding 계산 및 정규화
+        prompt_embeds = self.text_encoder(text_input_ids.to(device), attention_mask=attention_mask.to(device)).text_embeds
+        # additional L_2 normalization over each hidden-state
+        prompt_embeds = F.normalize(prompt_embeds, dim=-1).to(dtype=self.text_encoder.dtype, device=device)  # [B, 77, 768] >> ??
+        print(prompt_embeds.shape)
+
+        # 3. get unconditional embeddings for classifier free guidance
+        if do_classifier_free_guidance:
+            uncond_input = self.tokenizer(
+                [""] * batch_size,
+                padding="max_length",
+                max_length=prompt_embeds.shape[1],
+                truncation=True,
+                return_tensors="pt",
+            )
+            uncond_input_ids, attention_mask = uncond_input.input_ids.to(device), uncond_input.attention_mask.to(device)
+            uncond_prompt_embeds = self.text_encoder(uncond_input_ids, attention_mask=attention_mask).text_embeds
+            # additional L_2 normalization over each hidden-state
+            uncond_prompt_embeds = F.normalize(uncond_prompt_embeds, dim=-1)  # [B, 77, 768] >> ??
+            print(uncond_prompt_embeds.shape)
+
+            assert (uncond_prompt_embeds == uncond_prompt_embeds[0][None]).all()  # All the same
+            prompt_embeds = torch.cat([uncond_prompt_embeds, prompt_embeds])  # First B rows: uncond, Last B rows: cond
+        return prompt_embeds
+
     def train_step(self, text_embeddings: torch.Tensor, pred_rgb: torch.Tensor, guidance_scale: float = 100, t: Optional[int] = None):
         # Prepare image and timestep
         pred_rgb_512 = F.interpolate(pred_rgb, (512, 512), mode='bilinear', align_corners=False)
@@ -100,7 +148,7 @@ class StableDiffusion(nn.Module):
         noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
         # Calculate and apply gradients
-        w = (1 - self.alphas[t])
+        w = (1 - self.alphas_cumprod[t])
         grad = w * (noise_pred - noise)
         latents.backward(gradient=grad, retain_graph=True)
 
--- test.py
+++ test.py
@@ -8,10 +8,42 @@ from diffusers import AudioLDMPipeline
 
 import torch
 
-repo_id = "cvssp/audioldm"
+repo_id = "cvssp/audioldm"  # audioldm-s-full
+torch.set_float32_matmul_precision("highest")
 pipe = AudioLDMPipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
 pipe = pipe.to("cuda")
 
-prompt = "Techno music with a strong, upbeat tempo and high melodic riffs"
-audio = pipe(prompt, num_inference_steps=10, audio_length_in_s=5.0).audios[0]
-print(audio.shape)
+prompts = [
+    # 환경음 (Ambient & Nature Sounds)
+    "Waves crashing on a beach",
+    "Thunder rumbling in the distance",
+    "A rainforest with birds chirping and leaves rustling",
+    "A gentle breeze blowing through trees",
+    "A campfire crackling at night",
+
+    # 효과음 (Sound Effects / SFX)
+    "A cat meowing",
+    "A baby crying",
+    "Footsteps on a wooden floor",
+    "A door creaking open slowly",
+    "A spaceship engine humming",
+    "A robot beeping and glitching",
+
+    # 단순한 음악 (Simple Music Loops)
+    "A slow piano melody with soft chords",
+    "A fast-paced techno beat with deep bass",
+    "An 8-bit chiptune melody with simple drums",
+    "A relaxing lo-fi hip-hop beat with vinyl crackle",
+    "A synthwave melody with retro bass and dreamy pads"
+]
+
+audios = pipe(prompts, num_inference_steps=999, audio_length_in_s=10.24, guidance_scale=7.5).audios
+
+import soundfile as sf
+import re
+
+# audio의 길이가 5.0초이고, sample rate는 16000임
+for i, audio in enumerate(audios):
+    caption = prompts[i]
+    safe_filename = re.sub(r"[^\w\-_]", "_", caption)
+    sf.write(f"./samples/{safe_filename}.wav", audio, 16000)
